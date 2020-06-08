9e20bd68524603b6b3928b4785c8c5350ec395be
xanderMMR <40546745+xanderMMR@users.noreply.github.com>
2020-06-08 03:42:46 -0500

    Add files via upload

---
+++ Tarea4/MI_Zuul/src/main/java/pe/upc/zuul/MiZuulApplication.java
@@ -0,0 +1,17 @@
+package pe.upc.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class MiZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiZuulApplication.class, args);
+	}
+
+}
---
+++ Tarea4/MI_Zuul/src/test/java/pe/upc/zuul/MiZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.upc.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
